f2d24fdd5241f55ab3e6ed7e145b10a44e3e9abc
Kyle Custodio <kkzyc@yahoo.com>
2018-10-25 10:47:18 -0500

    skeleton for main logic

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -13,6 +13,11 @@
 using namespace std;
 
 BinaryTree<Node> createTree(ifstream &input);
+void readTransaction(ifstream &input, BinaryTree<Node> tree);
+void add(string line, BinaryTree<Node> tree);
+void remove(string line, BinaryTree<Node> tree);
+void rent(string line, BinaryTree<Node> tree);
+void returnMovie(string line, BinaryTree<Node> tree);
 
 int main()
 {
@@ -36,4 +41,50 @@ BinaryTree<Node> createTree(ifstream &input)
 		n->setRented(rent);
 	}
 	return tree;
+}
+
+void readTransaction(ifstream &input, BinaryTree<Node> tree)
+{
+	string line;
+	while (getline(input, line))
+	{
+		//check for proper format
+		if (line.rfind("add", 0) == 0)
+		{
+			add(line, tree);
+		}
+		else if (line.rfind("remove", 0) == 0)
+		{
+			remove(line, tree);
+		}
+		else if (line.rfind("rent", 0) == 0)
+		{
+			rent(line, tree);
+		}
+		else if (line.rfind("return", 0) == 0)
+		{
+			returnMovie(line, tree);
+		}
+	}
+}
+
+
+void add(string line, BinaryTree<Node> tree)
+{
+	cout << "add" << endl;
+}
+
+void remove(string line, BinaryTree<Node> tree)
+{
+	cout << "remove" << endl;
+}
+
+void rent(string line, BinaryTree<Node> tree)
+{
+	cout << "rent" << endl;
+}
+
+void returnMovie(string line, BinaryTree<Node> tree)
+{
+	cout << "return" << endl;
 }
